726d514ff56db2ba400e680498cf1dbad3d6f874
Samarth DevTools <samarthm@devtools.in>
2025-04-17 15:19:53 +0530

    Created sample.ts

---
+++ sample.ts
@@ -0,0 +1,11 @@
+let person = {
+    name: 'Samarth',
+    age: 20,
+    designation: 'Software Engineer'
+};
+
+console.log(person.name);
+
+
+let selectedColors = ['red', 'blue'];
+console.log(selectedColors);
